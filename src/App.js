0dd5277cb4c7e3731db8981e42cf937841c90a29
TomaszKorczyk <tomekkorczyk@interia.pl>
2021-01-08 20:02:16 +0100

    Commit Finish My First web App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React,{Component} from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/layout/Header'
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 import {v4 as uuidv4} from 'uuid';
 
 import './App.css';
@@ -55,14 +57,21 @@ class App extends Component {
   render(){
   // console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo addTodo={this.addTodo}/>
-          <h1>Hi Tomasz It`s great too see You ;)</h1>
-          <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <h1>Hi Tomasz It`s great too see You ;)</h1>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+              </React.Fragment>
+            ) } />
+            <Route path="/about" component={About} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
